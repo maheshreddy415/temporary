88fe2b83dacfe5706d4731ca32e74e888d43e6f1
Mahesh Kumar Parlapalli <mahesh.parlapalli@cognizant.com>
2016-08-08 20:20:59 +0530

    Delete jsfile.js

--- jsfile.js
+++
@@ -1,7 +0,0 @@
-CreateAccount.js
-------------------------------------------
-
-$('.switchBtn, .switchBtn_off').on('click', function(){
-	var thisButton = $(this);
-	thisButton.toggleClass('switchBtn_off');
-});
